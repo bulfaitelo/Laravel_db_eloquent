453383a022a7227b2c551d9313f2189366713221
Thiago Rodrigues <bulfaitelo@gmail.com>
2017-08-31 11:02:35 -0300

    Aula 12

---
+++ revendaAuto/database/migrations/2017_08_31_135822_create_carros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->string('descricao');
+            $table->integer('ano');
+            $table->decimal('valor', 5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carros');
+    }
+}
